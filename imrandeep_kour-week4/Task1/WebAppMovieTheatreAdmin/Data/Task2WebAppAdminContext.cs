2e2d2dd029ff7e28e3242edca2c46a659377abf3
imran1918 <70326846+imran1918@users.noreply.github.com>
2021-09-01 16:45:25 +0500

    Add files via upload

---
+++ imrandeep_kour-week4/Task1/WebAppMovieTheatreAdmin/Data/Task2WebAppAdminContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace WebAppMovieTheatreAdmin.Data
+{
+    public class WebAppMovieTheatreAdminContext : DbContext
+    {
+        public WebAppMovieTheatreAdminContext (DbContextOptions<WebAppMovieTheatreAdminContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; }
+        public DbSet<WebAppMovieTheatreAdmin.Models.Category> Category { get; set; }
+    }
+}
