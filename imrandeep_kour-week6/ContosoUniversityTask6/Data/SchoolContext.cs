391cd31843ed35fddb8a813f3871900e05a54562
imran1918 <70326846+imran1918@users.noreply.github.com>
2021-10-10 01:04:42 +0930

    Add files via upload

---
+++ imrandeep_kour-week6/ContosoUniversityTask6/Data/SchoolContext.cs
@@ -0,0 +1,23 @@
+﻿using ContosoUniversityTask6.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversityTask6.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
+    }
+}
---
+++ imrandeep_kour-week6/ContosoUniversityTask6/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ContosoUniversityTask6.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
